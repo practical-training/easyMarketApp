714dc3449c1d18f15c4dad56e313aac6fca2e5b5
dawei126156 <dawei126156@163.com>
2019-09-23 15:23:13 +0800

    router

---
+++ src/components/footer/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="footer">
+    <router-link to="/home">
+      <i></i>
+      <span>首页</span>
+    </router-link>
+    <router-link to="/special">
+      <i></i>
+      <span>专题</span>
+    </router-link>
+    <router-link to="/classify">
+      <i></i>
+      <span>分类</span>
+    </router-link>
+    <router-link to="/car">
+      <i></i>
+      <span>购物车</span>
+    </router-link>
+    <router-link to="/mine">
+      <i></i>
+      <span>我的</span>
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
---
+++ src/views/classify/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="home">
+     classify
+  </div>
+</template>
+
+<style lang="scss">
+
+
+</style>
