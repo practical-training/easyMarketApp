76ff7fb326a3f5b0679dfba0f16809ed2bf6a6ce
meimei <1254630731@qq.com>
2019-09-23 20:07:25 +0800

    store

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
-
+<script>
+import { mapActions,mapMutations,mapState } from "vuex";
+export default {
+  data() {
+    return {};
+  },
+  computed:{
+    ...mapState("home",{
+      homeData:"data"
+    })
+  },
+  methods: {
+    ...mapActions("home",["updateValue"])
+  },
+  mounted() {
+   this.updateValue()
+  }
+};
+</script>
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
