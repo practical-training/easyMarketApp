be72be4ef92db21c506e99b32a3fe9d4f928fc3b
luxuan <1131576937@qq.com>
2019-09-27 20:40:52 +0800

    luxuan 1.4

--- src/views/mine/addresslist.vue
+++ src/views/mine/addresslist.vue
@@ -5,17 +5,24 @@
             <h4>地址管理</h4>
         </div>
         <div class="addressPage" ref="itemScroll">
-            <div class="addressList">
+            <div class="addressList" v-if="!flag">
                 <Addressitem />
                 <Addressitem />
                 <Addressitem />
+            </div>
+            <div class="addressSetPage" v-if="flag">
+                <div class="onePx_bottom"><input class="addressInput" placeholder="姓名"></div>
+                <div class="onePx_bottom"><input class="addressInput" placeholder="电话号码"></div>
+                <div class="onePx_bottom"><div class="chooseAddress">北京/北京市/东城区</div></div>
+                <div class="onePx_bottom"><input class="addressInput" placeholder="详细地址"></div>
+                <div class="onePx_bottom"><div class="isDefaultAddress">设置默认地址<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAmCAMAAACf4xmcAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAABCUExURUdwTMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzAV+Z0EAAAAVdFJOUwAJ+KUEFTPay2bzRXdZ7RkhmJ6qJOWhY+QAAAEDSURBVDjLnZTplsIgDIUNWwK2tdt9/1cdxHGmVcAc+dH25Hw0+71cvjhztDIZM4mNc4txo+BwZKxSVwbSFoMn8iFuCeDrG0RLNkc6GGK+ttCZ8gIzuJcgBgPxJ4rB4T2OkM0HjgRyq8V7Y8i/3/V06YVb/nKECa0qBYPffB1jaFd8AD8+RrBrY8R41FkQew2MkPtrR6IeRglzoW1/HrbizfZ9Pv8jCH0slOAm+D7mMeUn4PoYwegxpVNlCsqCKMurbJay9R8GyT0HSTmWeciTYsh7K+MPK1MW0H9eQOU652sqcch+15rUrFQXLpuFy7ksXLYuXDUZbBZ9v4sqiqju34jyD97JD4dkfgo1AAAAAElFTkSuQmCC" alt="check"></div></div>
             </div>
         </div>
         <div class="addAddress">
-            <div class="am-button" v-if="!flag">
+            <div class="am-button" v-if="!flag" @click="addlist">
                 <span>新增地址</span>
             </div>
-            <div v-if="flag" class="am-button-inline"><span>取消</span></div>
+            <div v-if="flag" class="am-button-inline" @click="dellist"><span>取消</span></div>
             <div v-if="flag" class="am-button-primary"><span>保存</span></div>
         </div>
     </div>
@@ -34,7 +41,9 @@ export default {
     },
     data(){
         return {
-            flag:false
+            flag:false,
+            flagimg:'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAmCAMAAACf4xmcAAAAQlBMVEUAAACrKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyvw19exOzv////z4uK1Q0Pt0dGxOjp+CNkCAAAADnRSTlMARVn7B9cVoc/jIWtnJIM++AMAAADUSURBVDjLndRLEoMgEEVRPyCg+FAh+99qYqmAabFL7/hMaKCrN/VWyRZopbJ9ETUaWbq5RLXBX6YmSChcpMRZdRKX6e6kDAqZzAmNYlpEpnCTimfEbfWmhLlnZp8qmLY5a47pVY0oNIWArfV+h5Jy88FsNg2q3JTNRLIK8sd4hTZnwfmzSuVsmRdPFGV+d1S18QjJUQUZB5IcVVBxvMlRBRsvKzmq0JOr9y58yNU/eEj8s3zyyPkvcyQk9wH57/xwOfCrhl9cNMGswdQ4HEt1GKsXfQHGSThPkNi75AAAAABJRU5ErkJggg',
+            
             
         }
     },
@@ -48,6 +57,12 @@ export default {
         goto() {
       this.$router.go(-1);
     },
+    addlist(){
+        this.flag=true;
+    },
+    dellist(){
+        this.flag=false;
+    }
     },
     created(){
     },
@@ -70,6 +85,9 @@ export default {
         }
       });
     });
+    },
+    watch(){
+        
     }
 }
 </script>
--- src/views/mine/components/addressitem.vue
+++ src/views/mine/components/addressitem.vue
@@ -3,7 +3,11 @@
         <div class="isChooseAddress"></div>
         <div class="addressMsg">
             <div class="concatName">大</div>
-            <div class="addressDetail"></div>
+            <div class="addressDetail">
+                <div class="concatPhone">13546546796</div>
+                <div class="concatAddress">安徽省合肥市瑶海区</div>
+                <div class="concatAddress">11</div>
+            </div>
             <div class="deleteAddress">
                 <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyBAMAAADsEZWCAAAALVBMVEVmZmZtbW1ubm5wcHCJiYmKioqOjo6Tk5OUlJSVlZXm5ubn5+fv7+/y8vL////PAON/AAAAa0lEQVQ4y2N4hwsw0F8mgIGBgRWrDAMIoMokMKADNoIy6O5gwCAHgUwoThkGEmWexiFTyDIP+JCpUZmBkXnqh0wRH9shA5dGBe4hSbxlRJJpqEGSOcmBJLMYJS9aIcm8noQkobluoModNAAADGfUnhuYO1kAAAAASUVORK5CYII=" alt="">
             </div>
