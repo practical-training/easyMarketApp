77997204ba9ccda976df4a307ec8753de6c2c356
luxuan <1131576937@qq.com>
2019-09-28 15:22:15 +0800

    luxuan 1.7

---
+++ src/store/modules/product.ts
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import { productDetil} from "../../service";
+Vue.use(Vuex);
+export default ({
+  namespaced: true,
+  state: {
+    data:{}
+  },
+  mutations: {
+    setDetaildata(state:any, payload:any) {
+      state.data = payload.data.data
+      console.log(payload.data.data,"4444")
+    }
+  },
+  actions: {
+    async productDetil({commit}:any, payload:any) {
+      let data = await productDetil(payload);
+      commit("setDetaildata", { data });
+    }
+  }
+});
